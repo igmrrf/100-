c6a8c12c93a0e0cd44504f0a43db5cfc7569268e
The Lazy Dev Otaku <francis.igbiriki.fi@gmail.com>
2020-05-14 19:04:40 +0100

    OffLoad

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ async function registerServiceWorker() {
 		}
 	}
 }
+
+//Sending an Ajax request to a file within the system
+// var xhttp = new XMLHttpRequest();
+// xhttp.onreadystatechange = function () {
+// 	if (this.readyState == 4 && this.status == 200) {
+// 		var response = xhttp.responseText;
+
+// 		var output = "";
+// 		for (var i = 0; i < response.length; i++) {
+// 			output += "<li>" + response[i] + "</li>";
+// 		}
+// 		document.getElementById("people").innerHTML = output;
+// 	}
+// };
+// xhttp.open("GET", "people.json", true);
+// xhttp.send();
